3e0fc5b180f39e4a321cb79f74bdfa3b84b067a7
Victor tondolo <victor.tondolo@gmail.com>
2021-11-13 19:38:08 +0200

    Ugrade .env.example and enable login and register
    Token gestion in sessionStorage

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,19 +2,35 @@ import { useRef } from 'react';
 import { useHistory } from 'react-router-dom';
 import FieldItem from './FieldItem';
 import './Form.css';
+import axios from 'axios';
 
 const LoginForm = () => {
     let history = useHistory();
     const emailRef = useRef();
     const passwordRef = useRef();
 
-    const handleSubmit = e => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         const data = {
             email: emailRef.current.value,
             password: passwordRef.current.value
         };
         console.log(data);
+        
+        await axios({
+          method: 'post',
+          url: 'http://localhost:3000/api/login',
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+          },
+          data: data
+        }).then((response) => {
+          localStorage.setItem('__TOKEN__', response.data);
+          history.push('/home');
+        }).catch((error) => {
+            console.error('There was an error!', error);
+        });
     };
 
     return (
